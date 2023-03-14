b8bb33ba3e964174bef5e539c10ff8c4eca839d0
Tobi <tobi@tobipapenbrock.de>
2023-03-14 20:26:43 +0100

    Erweiterung: Rabattprüfung anhand der Farbe

--- Preisrechner.py
+++ Preisrechner.py
@@ -2,11 +2,13 @@
 # Initialisierung 
 anzahl          = int(input("Bitte die Anzahl eingeben: "))
 einzel_preis    = float(input("Bitte den Einzelpreis eingeben:"))
+farbe           = input("Welche Farbe: ")
 rabatt          = 0
 
 summe = anzahl * einzel_preis
 print(summe)
 
+#Berechnung Rabatte abhängig von der Mänge 
 if anzahl >=40 and anzahl % 10 == 0:
     rabatt = 0.95
 elif anzahl >= 31 and einzel_preis > 10:
@@ -18,6 +20,10 @@ elif anzahl > 6:
 else:
     rabatt = 0
 
+#Berechnung Rabatte abhängig von der Farbe 
+
+if farbe == "rot" or farbe =="blau":
+    summe*=0.9
 
 summe = summe * rabatt
 #alternativ summe*=rabatt
