a0e83d40e8be86e9e623de7d227a4bd829a51db2
Tobi <tobi@tobipapenbrock.de>
2023-03-14 19:06:46 +0100

    Aufgabe 1

---
+++ P01.py
@@ -0,0 +1,18 @@
+# Initialisierung
+geheimzahl  = 4712
+eingabe     = 0
+zaehler     = 0
+
+# Schleifenkopf und Schleifenkoerper
+while eingabe != geheimzahl:
+    eingabe = int(input("Ganze Zahl eingeben: "))
+
+    if eingabe < geheimzahl:
+        print("Zahl zu klein")
+
+    if eingabe > geheimzahl:
+        print("Zahl zu groß")
+
+    zaehler = zaehler +1
+
+print ("Richtig! Sie haben", zaehler, "Versuche benötigt.")
