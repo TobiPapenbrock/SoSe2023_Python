697ea81a0f1baf65e5aa019c24c9964caaf1d373
Tobi <tobi@tobipapenbrock.de>
2023-03-14 20:14:50 +0100

    kleine Anpassungen

--- Preisrechner.py
+++ Preisrechner.py
@@ -5,17 +5,19 @@ einzel_preis    = float(input("Bitte den Einzelpreis eingeben:"))
 rabatt          = 0
 
 summe = anzahl * einzel_preis
+print(summe)
 
 if anzahl >= 31:
-    rabatt = 0.04
-elif anzahl > 16 and anzahl < 30:
-    rabatt = 0.03
-elif anzahl > 6 and anzahl < 15:
-    rabatt = 0.02
+    rabatt = 0.96
+elif anzahl > 16:
+    rabatt = 0.97
+elif anzahl > 6:
+    rabatt = 0.98
 else:
     rabatt = 0
 
-summe = summe - (summe * rabatt)
+summe = summe * rabatt
+#alternativ summe*=rabatt
 
 print("Das kostet in der Summe", summe, "€")
 print("Mit einem Rabatt von", summe * rabatt, "€")
