ae0e01189a048f58408be446cba3d2b087344f72
Tobi <tobi@tobipapenbrock.de>
2023-03-15 20:52:54 +0100

    lala

---
+++ kreis.py
@@ -0,0 +1,7 @@
+pi =3.141
+#durchmesser =int(input("Durchmesser: "))
+#durchmesser = 1e100
+durchmesser = float(input("Durchmesser: "))
+umfang = durchmesser * pi
+print(umfang)
+print(format(umfang, ".30g"))
